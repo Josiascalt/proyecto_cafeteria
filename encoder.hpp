664d475a21fd1f27dc5595234830a90a112047a8
josiascabreraaltamirano <xosi.000901@icloud.com>
2024-11-18 13:27:28 +0800

    Encode is being coded

--- encoder.hpp
+++ encoder.hpp
@@ -3,21 +3,62 @@
 #include <string>
 #include <array>
 #include <algorithm>
+#include <cmath>
 #include <cstdint>
 
 namespace encoder {
-    static const int UINT32_IN_TYPE128 = 4;
-    using EncodingType128 = std::array<uint32_t, UINT32_IN_TYPE128>;
+    static const int RELATIVE_POSITION = 32;
+
+    static const int UINT64_CAPACITY = 9;
+    static const int UINT64_IN_TYPE128 = 2;
+    using Type128 = std::array<uint64_t, UINT64_IN_TYPE128>;
 
     //String encoder in 16 bytes - any char from 32 - 126
-    //The string size should be 20 or less
-    EncodingType128 Encode128(const std::string& str) {
-        EncodingType128 result;
+    Type128 Encode128(const std::string& str) {
+        Type128 result({0,0});
+        auto result_index = 0;
         const int size = str.size();
+        const int digit_position_step = 2;
+
+        int digit_position = 0;
         for (int i = 0; i < size;) {
-            auto step = std::min(UINT32_IN_TYPE128, size - i);
-            /*some code here*/
-            i += step;
+            auto step = std::min(UINT64_CAPACITY, size - i);
+
+            if (step == UINT64_CAPACITY) {
+                result[result_index] += static_cast<uint64_t>(std::pow(10, digit_position) * (static_cast<int>(str[i++]) - RELATIVE_POSITION));
+                digit_position += digit_position_step;
+
+                result[result_index] += static_cast<uint64_t>(std::pow(10, digit_position) * (static_cast<int>(str[i++]) - RELATIVE_POSITION));
+                digit_position += digit_position_step;
+
+                result[result_index] += static_cast<uint64_t>(std::pow(10, digit_position) * (static_cast<int>(str[i++]) - RELATIVE_POSITION));
+                digit_position += digit_position_step;
+
+                result[result_index] += static_cast<uint64_t>(std::pow(10, digit_position) * (static_cast<int>(str[i++]) - RELATIVE_POSITION));
+                digit_position += digit_position_step;
+
+                result[result_index] += static_cast<uint64_t>(std::pow(10, digit_position) * (static_cast<int>(str[i++]) - RELATIVE_POSITION));
+                digit_position += digit_position_step;
+
+                result[result_index] += static_cast<uint64_t>(std::pow(10, digit_position) * (static_cast<int>(str[i++]) - RELATIVE_POSITION));
+                digit_position += digit_position_step;
+
+                result[result_index] += static_cast<uint64_t>(std::pow(10, digit_position) * (static_cast<int>(str[i++]) - RELATIVE_POSITION));
+                digit_position += digit_position_step;
+
+                result[result_index] += static_cast<uint64_t>(std::pow(10, digit_position) * (static_cast<int>(str[i++]) - RELATIVE_POSITION));
+                digit_position += digit_position_step;
+
+                result[result_index] += static_cast<uint64_t>(std::pow(10, digit_position) * (static_cast<int>(str[i++]) - RELATIVE_POSITION));
+                digit_position += digit_position_step;
+
+                digit_position = 0;
+                result_index++;
+                continue; 
+            }
+
+            result[result_index] += static_cast<uint64_t>(std::pow(10, digit_position) * (static_cast<int>(str[i++]) - RELATIVE_POSITION));
+            digit_position += digit_position_step;
         }
 
         return result;
