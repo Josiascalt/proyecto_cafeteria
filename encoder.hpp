09e126ed42d79ecdff72baa46cb8a86a0021c821
Josiascalt <xosi.000901@gmail.com>
2024-11-20 14:00:14 +0800

    adding encoding features

--- encoder.hpp
+++ encoder.hpp
@@ -5,6 +5,8 @@
 #include <cstdint>
 #include <vector>
 #include <iterator>
+#include <utility>
+#include <memory>
 
 namespace encoder {
     class Item {
@@ -35,10 +37,48 @@ namespace encoder {
         Type value_ = 0;
     };
 
-    using EncodedData = std::vector<Item>;
+
+    class Encoder {
+    public:
+        Encoder() = default;
+
+        Encoder(const std::string& str) 
+        : data_(nullptr)
+        {   
+        }
+
+        template <typename Iter>
+        Encoder(Iter beg, Iter end) 
+        : data_(nullptr)
+        {
+        }
+
+        static int CalcDataSizeInItems(int data_size) {
+            return std::ceil(data_size / double(Item::CAPACITY));
+        }
+
+        template <typename OutIter, typename InputIter>
+        static void EncodeData(InputIter beg
+                  , InputIter end
+                  , OutIter target
+                  , int target_capacity) {
+
+            auto iter_begin = beg;
+            for (int i = 0; i < target_capacity - 1; i++) {
+                auto iter_end = iter_begin + Item::CAPACITY;
+                *target++ = std::move(Item{iter_begin, iter_end});
+                iter_begin = iter_end;
+            }
+
+            *target = std::move(Item{iter_begin, end});
+        }
+
+    private:
+        std::unique_ptr<Item> data_;
+    };
     
-    inline EncodedData EncodeData(const std::string& data) {
-        EncodedData result;
+    /*inline Encoder EncodeString(const std::string& data) {
+        Encoder result;
         int size_in_items = std::ceil(data.size() / double(Item::CAPACITY));
         result.reserve(size_in_items);
 
@@ -52,5 +92,5 @@ namespace encoder {
         result.emplace_back(begin, data.end());
 
         return result;
-    }
+    }*/
 } // namespace encoder
